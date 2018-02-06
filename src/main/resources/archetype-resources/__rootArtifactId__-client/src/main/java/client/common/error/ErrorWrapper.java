f9f6d7de851bed22eb0e4cfbfa41202c361c5f7b
zhangyan <578033593@qq.com>
2018-02-06 20:29:28 +0800

    fix

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/common/error/ErrorWrapper.java
@@ -0,0 +1,12 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.client.common.error;
+
+
+
+/**
+ * Created by ${userName} on ${today}.
+ */
+public class ErrorWrapper  {
+}
