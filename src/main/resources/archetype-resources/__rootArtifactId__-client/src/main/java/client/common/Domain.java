704b5937f4ab2c53b1ed1e534560e3404b58747d
zhangyan <578033593@qq.com>
2018-02-06 20:07:15 +0800

    fixBug

--- src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/common/Domain.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/common/Domain.java
@@ -1,7 +1,7 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.client.common.domain;
+package ${package}.client.common;
 
 
 
--- src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/java/core/user/service/UserInfoService.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-core/src/main/java/core/user/service/UserInfoService.java
@@ -3,12 +3,11 @@
 #set( $symbol_escape = '\' )
 package ${package}.core.user.service;
 
-import ${package}.client.common.list.ListWrapper;
-import ${package}.client.common.result.Result;
+
 
 /**
  * Created by ${userName} on ${today}.
  */
 public interface UserInfoService {
-    Result<ListWrapper<String>> findUserNameByDesc(Integer pageSize, Integer currentPage);
+   
 }
