9aa727a9e5c5e3cf675b97428832c72ea007072c
zhangyan <578033593@qq.com>
2018-02-06 21:25:21 +0800

    fix

--- src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/common/error/ErrorWrapper.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/common/error/ErrorWrapper.java
@@ -3,10 +3,28 @@
 #set( $symbol_escape = '\' )
 package ${package}.client.common.error;
 
-
+import lombok.Data;
 
 /**
  * Created by ${userName} on ${today}.
  */
-public class ErrorWrapper  {
+@Data
+public class ErrorWrapper {
+    private long code;
+    private String name;
+    private String msg;
+
+    public ErrorWrapper() {
+        this(200, "EMPTY", "EMPTY");
+    }
+
+    public ErrorWrapper(long code, String name, String msg) {
+        this.code = code;
+        this.name = (name == null || name.trim().equals("")) ? "EMPTY" : name;
+        this.msg = (msg == null || msg.trim().equals("")) ? "EMPTY" : msg;
+    }
+
+    public String toJson(){
+        return String.format("{\"code\":%s,\"name\":\"%s\",\"msg\":\"%s\"}", code, name, msg);
+    }
 }
