e3e9a51ad51b26ddc5797d1918f47a01d6d85ed1
zhangyan <578033593@qq.com>
2018-02-12 18:07:39 +0800

    fix

--- src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/common/domain/Domain.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/common/domain/Domain.java
@@ -1,12 +1,45 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-package ${package}.client.common.domain;
+package $
+
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.CompareToBuilder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+{package}.client.common.domain;
 
 
 
 /**
  * Created by ${userName} on ${today}.
  */
-public class Domain  {
+public class Domain  implements Serializable, Cloneable, Comparable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1093611608004530526L;
+	
+	
+	public String toString() {
+        return ToStringBuilder.reflectionToString(this,
+                ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this, o);
+    }
+    
+    public int hashCode() {
+    	return HashCodeBuilder.reflectionHashCode(this);
+    }
+    
+    public int compareTo(Object o) {
+        return CompareToBuilder.reflectionCompare(this, o);
+    }
 }
