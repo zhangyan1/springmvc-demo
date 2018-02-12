3adc83b0f14a610a1e7b8615a4cebdcea3336b28
zhangyan <578033593@qq.com>
2018-02-12 18:20:05 +0800

    fix

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-client/src/main/java/client/common/list/ListVO.java
@@ -0,0 +1,59 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.client.common.list;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+
+/**
+ * Created by ${userName} on ${today}.
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class ListVO<Value> {
+	
+    private List<Value> rows;
+    private Long total;
+    private Long currentPage;
+    private Long pageSize;
+
+    public static <T> ListVO<T> list(Result<List<T>> ret) {
+        return list(ret.getValue(), ret.getRemark());
+    }
+
+    public static <T> ListVO<T> list(List<T> list, String remark) {
+        return list(list, VerifyUtils.isInteger(remark) ? Long.parseLong(remark) : null);
+    }
+
+    public static <T> ListVO<T> list(List<T> list, Integer totalCount) {
+        return list(list, totalCount == null ? null : totalCount.longValue());
+    }
+
+    public static <T> ListVO<T> list(List<T> list, Long totalCount) {
+        return list(list, totalCount, null, null);
+    }
+
+    public static <T> ListVO<T> list(List<T> list, Long totalCount, Long currentPage, Long pageSize) {
+        ListVO<T> vos = new ListVO<>();
+        vos.setRows(list);
+        vos.setTotalCount(totalCount != null ? totalCount : (vos.rows == null ? 0 : vos.rows.size()));
+        vos.setCurrentPage(currentPage);
+        vos.setPageSize(pageSize);
+        return vos;
+    }
+}
--- src/main/resources/archetype-resources/__rootArtifactId__-dal/src/main/java/dal/user/wrapper/impl/UserInfoWrapperImpl.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-dal/src/main/java/dal/user/wrapper/impl/UserInfoWrapperImpl.java
@@ -1,24 +1,14 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
+
 package ${package}.dal.user.wrapper.impl;
 
-import ${package}.client.common.error.ErrorInfo;
-import ${package}.client.common.exception.DatabaseSqlExecuteException;
-import ${package}.client.common.list.ListWrapper;
-import ${package}.client.common.result.Result;
-import ${package}.client.common.result.ResultFactory;
-import ${package}.client.user.domain.UserInfoDomain;
-import ${package}.client.user.query.UserInfoQuery;
-import ${package}.dal.common.wrapper.MapperWrapper;
-import ${package}.dal.user.mapper.UserInfoMapper;
 import ${package}.dal.user.wrapper.UserInfoWrapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import javax.annotation.Resource;
-import java.util.List;
-import java.util.Random;
+
 
 /**
  * Created by ${userName} on ${today}.
